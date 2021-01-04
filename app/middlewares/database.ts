cd0d6d1f582c644e88bd71893fb04b22afc3f1ad
nicholas <nicholasdegrate@Nicholass-Mac-mini.local>
2021-01-03 20:27:46 -0500

    fix bugs

---
+++ app/middlewares/database.ts
@@ -0,0 +1,14 @@
+import { MongoClient } from 'mongodb';
+
+const client = new MongoClient(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+export default async function database(req, res, next) {
+    if (!client.isConnected()) await client.connect();
+    req.dbClient = client;
+    req.db = client.db(process.env.DB_NAME);
+    await setUpDb(req.db);
+    return next();
+}
