c44c8355fb8d258769b56de84dac0d75f771fc57
Jamison Blackwell <jamie@Jamisons-MacBook-Pro.local>
2021-01-28 04:04:03 -0500

    dashboard is styled

--- app/pages/dashboard.tsx
+++ app/pages/dashboard.tsx
@@ -1,6 +1,8 @@
 import React,{useState} from 'react';
-import TopDashboard from './topDashBoard';
+import TopDashboard from './topDashboard';
 import MidDashboard from './midDashboard';
+import Bottomdashboard from './bottomDashboard';
+import Navbar from './navbar';
 
 
 
@@ -9,9 +11,33 @@ const Dashboard = () => {
 
   return (
     <React.Fragment>
-      <TopDashboard />
-      <MidDashboard />
+      <div className='full-dashboard-contain'>
+        <section className='stock-bar'>
+
+<div className="ticker-wrap">
+<div className="ticker">
+  <div className="ticker__item">Letterpress chambray brunch.</div>
+  <div className="ticker__item">Vice mlkshk crucifix beard chillwave meditation hoodie asymmetrical Helvetica.</div>
+  <div className="ticker__item">Ugh PBR&B kale chips Echo Park.</div>
+  <div className="ticker__item">Gluten-free mumblecore chambray mixtape food truck.
+  </div>
+  </div>
+  </div>
+
+        </section>
+        <div className='nav-dash'>
+          <div className='nav-holder'>
+            <Navbar />
+          </div>
+          <section className='dashboard-container'>
+            <TopDashboard />
+            <MidDashboard />
+            <Bottomdashboard />
+          </section>
+        </div>
+      </div>
     </React.Fragment>
+    
   )
 
 }
